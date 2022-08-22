69ea629d22e7e65006f902f8090cceef3ced3d8a
Archi Mittal <78952113+Archi24@users.noreply.github.com>
2022-08-22 19:36:36 +0530

    Update Challenge2.cpp

--- Challenge2.cpp
+++ Challenge2.cpp
@@ -1,35 +1,117 @@
-#include <iostream>
-#include <string>
-#include <fstream>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-#include "expression.h"
-#include "subexpression.h"
-#include "symboltable.h"
-#include "parse.h"
+bool isChar(string s)
+{
+     if(s.size() >1 ) return false;
 
-SymbolTable symbolTable;
+     switch (s[0]) {
+        case '+': return true;
+        case '-': return true;
+        case '*': return true;
+        case '/': return true;
+        case '(': return true;
+        case ')': return true;
+        case '^': return true;
+        default: return false;
+    }
+}
 
-void parseAssignments();
+int solve(int op1, int op2, char ch)
+{
+     switch(ch)
+     {
+          case '+': return (op1+op2);
+          case '-': return (op1-op2);
+          case '*': return (op1*op2);
+          case '/': return (op1/op2);
+          case '^': return pow(op1,op2);
+
+     }
+}
+
+int preference( char ch)
+{
+     switch(ch)
+     {
+          case '+': return 1;
+          case '-': return 1;
+          case '*': return 2;
+          case '/': return 2;
+          case '^': return 3;
+          case '(': return -1;
+     }
+}
+
+void evaluate(string s1)
+{
+     stack<char> operators;
+     stack<int> operands;
+
+     string s;
+     istringstream is(s1);
+     while(is>>s)
+     {
+          if(isChar(s))
+          {
+               if(s[0]=='(') operators.push('(');
+               else if(s[0]==')')
+               {
+                    while(operators.top()!='(')
+                    {
+                         char ch=operators.top(); operators.pop();
+                         int op2=operands.top(); operands.pop();
+                         int op1=operands.top(); operands.pop();
+
+                         operands.push(solve(op1,op2,ch));
+                    }
+                    operators.pop();
+               }
+               else
+               {
+                    while(!operators.empty() &&   preference(s[0]) <= preference(operators.top()))
+                    {
+                         char ch=operators.top(); operators.pop();
+                         int op2=operands.top(); operands.pop();
+                         int op1=operands.top(); operands.pop();
+
+                         operands.push(solve(op1,op2,ch));
+
+                    }
+
+                    operators.push(s[0]);
+               }
+
+          }
+          else
+          {
+               int op=atoi(s.c_str());
+               operands.push(op);
+          }
+
+     }
+
+     while(!operators.empty())
+     {
+          char ch=operators.top(); operators.pop();
+          int op2=operands.top(); operands.pop();
+          int op1=operands.top(); operands.pop();
+
+          operands.push(solve(op1,op2,ch));
+     }
+
+     //cout<<operands.empty()<<" "<<operators.empty();
+     cout<<"result: "<<operands.top()<<endl;
+}
 
 int main()
 {
-    Expression* expression;
-    char paren, comma;
-    string program;
+     int t;
 
-    ifstream myfile("expressions.txt");
+          string s="2 + 3 * 5 + ( 2 ^ 3 )";
+          //cin>>s;
+          evaluate(s);
 
-    if (myfile.is_open())
-    {
-        while (getline(myfile, program))
-        {
-            cout << program << '\n';   
-        }
-        myfile.close();
-    }
-    else cout << "Unable to open file";
-
-    cin.get(); 
-    return 0;
+     return 0;
 }
